aa1b615d8b2d020809defe71ad45ffc05480df5e
Yi Wang <wajulia@gmail.com>
2010-02-28 01:22:41 -0500

    wire up followup & demography

--- end.js
+++ end.js
@@ -1,8 +1,9 @@
-;jQuery(document).ready(function(){  
-    jQuery("#submit-view").bind("submit", function(event){
-      event.preventDefault();
-    });
-    jQuery('#submit-view').validate({
+;
+var cstage = null;
+jQuery(document).ready(function(){  
+    jQuery('#followup-form').validate();
+    jQuery('#demography-form').validate();
+    jQuery('#final-form').validate({
       rules: {
         email: {
           required: true,
@@ -10,27 +11,13 @@
         }
       }
     });
-    var len = followup['valid-question-ids'].length;
-    for(var i=0;i<len;i++){
-      var i_str = followup['valid-question-ids'][i];
-      jQuery('#followup-'+i_str+'-desc').html(followup['questions'][i_str][lang]);
-      //jQuery('#followup-'+i_str).html(i_str+'. '+followup['questions'][i_str][lang]);
-      for(var j=1;j<=5;j++){
-        jQuery('#followup-'+i_str+'-answer-'+j).html(followup['answers'][lang][j-1]);  
-      }
-      jQuery('input[name="followup-'+i_str+'"]').rules("add", {required: true});
-    }
-    
-    
-    jQuery('#end-form').html(end['end-form-text'][lang]);
-    jQuery('#confirmation-number').html(cnum_str);
-    jQuery('#email-form').html(end['email-form-text'][lang]);
-    jQuery('#comment-end-label').html(end['comment-end-label'][lang]);
-    jQuery('#submit').attr({value: end['submit-text'][lang]});
+    jQuery('#header').html(end.header[lang]);
+    fill_html_followup();
+    fill_html_demography(); jQuery('#demography-form').hide();
+    fill_html_final(); jQuery('#final-form').hide();
+    cstage = 0;
     jQuery('#submit').click(function(){
-      if(jQuery('#submit-view').valid()){
-        jQuery.post('end-save.php',{entry: Object.toJSON([cnum_str,jQuery('input[name="email"]').attr('value'),jQuery('#comment-end-input').attr('value').replace(/,/g,";"),ip,navigator.userAgent])},show_final,'text');
-      }
+      next();
     });
     jQuery('*').keydown(function(e){
     if(e.which==13){
@@ -40,8 +27,87 @@
     
   });    
 });
+function flat_end(){
+  var r = [cnum_str,jQuery('input[name="email"]').attr('value')];
+  r.push('FOLLOWUP');
+  var len = followup['valid-question-ids'].length;
+  for(var i=0;i<len;i++){
+    r = r.concat();
+    var i_str = followup['valid-question-ids'][i];
+    r.push(jQuery('input[name="followup-'+i_str+'"]:checked').attr('value'));
+  }
+  r.push('DEMOGRAPHY');
+  len = demography['valid-question-ids'].length;
+  for(var i=0;i<len;i++){
+    var i_str = demography['valid-question-ids'][i];
+    r.push(demography['questions-answers'][i_str]['answer'][lang][jQuery('input[name="demography-'+i_str+'"]:checked').attr('value')-1].replace(/,/g,";"));
+  }  
+  r.push('COMMENT');
+  r = r.concat(jQuery('#comment-end-input').attr('value').replace(/,/g,";"));
+  r.push('IP');
+  r = r.concat([ip,navigator.userAgent]);
+  return r;
+}
+function next(){
+  if(cstage==0){
+    if(jQuery('#followup-form').valid()){
+      jQuery('#followup-form').hide();
+      jQuery('#demography-form').show();
+      cstage=1;
+    } 
+    return;
+  }
+  if(cstage==1){
+    if(jQuery('#demography-form').valid()){
+      jQuery('#demography-form').hide();
+      jQuery('#final-form').show();
+      cstage=2;
+    } 
+    return;
+  }
+  if(cstage==2){
+    if(jQuery('#final-form').valid()){
+      jQuery('#submit').hide();
+      jQuery.post('end-save.php',{entry: Object.toJSON(flat_end())},show_final,'text');
+    }
+    return;
+  }
+}
 function show_final(res){
-  jQuery('#submit-view').hide();
+  jQuery('#header').hide();
+  jQuery('#final-form').hide();
   jQuery('#result').html(res);
   jQuery('#final-view').html(end['final-view-text'][lang]);
+}
+function fill_html_followup(){
+    jQuery('#followup-desc').html(followup['desc'][lang]);
+    var len = followup['valid-question-ids'].length;
+    for(var i=0;i<len;i++){
+      var i_str = followup['valid-question-ids'][i];
+      jQuery('#followup-'+i_str+'-desc').html(i_str+'. '+followup['questions'][i_str][lang]+'.');
+      for(var j=1;j<=5;j++){
+        jQuery('#followup-'+i_str+'-answer-'+j).html(followup['answers'][lang][j-1]);  
+      }
+      jQuery('input[name="followup-'+i_str+'"]').rules("add", {required: true});
+    }
+}
+function fill_html_demography(){
+    jQuery('#demography-desc').html(demography['desc'][lang]);
+    var len = demography['valid-question-ids'].length;
+    for(var i=0;i<len;i++){
+      var i_str = demography['valid-question-ids'][i];
+      jQuery('#demography-'+i_str+'-desc').html(i_str+'. '+demography['questions-answers'][i_str]['question'][lang]);
+      var count = demography['questions-answers'][i_str]['answer'][lang].length;
+      for(var j=1;j<=count;j++){
+        jQuery('#demography-'+i_str+'-answer-'+j).html(demography['questions-answers'][i_str]['answer'][lang][j-1]);  
+      }
+      jQuery('input[name="demography-'+i_str+'"]').rules("add", {required: true});
+    }
+}
+function fill_html_final(){
+    jQuery('#confirmation-number').html(cnum_str);
+    jQuery('#end-desc').html(end['end-desc'][lang]);
+    jQuery('#email-desc').html(end['email-desc'][lang]);
+    jQuery('#comment-end-label').html(end['comment-end-label'][lang]);
+    jQuery('#submit').attr({value: end['nav']['next'][lang]});
 }
--- end.php
+++ end.php
@@ -8,6 +8,7 @@ if(empty($lang)){
   $lang = 'en';
 }
 //echo $lang;
+$debug = intval($_GET['debug']);
 $cnum_str = $_GET['cnum_str'];
 $et = json_decode(stripslashes($_POST['entry']), true);
 //*
@@ -42,9 +43,14 @@ echo 'end='.json_encode($end).';';
 echo 'lang="'.$lang.'";';
 echo 'cnum_str="'.$cnum_str.'";';
 echo 'ip="'.$_SERVER['REMOTE_ADDR'].'";';
+echo 'debug='.$debug.';';
 
 $followup = Spyc::YAMLLoad('conf.d/followup.yaml');
 $demography = Spyc::YAMLLoad('conf.d/demography.yaml');
+if($debug){
+  $followup['valid-question-ids'] = array($followup['valid-question-ids'][0]);
+  $demography['valid-question-ids'] = array($demography['valid-question-ids'][0]);
+}
 echo 'followup='.json_encode($followup).';';
 echo 'demography='.json_encode($demography).';';
 ?>  
@@ -71,15 +77,19 @@ pageTracker._trackPageview();
 } catch(err) {}</script>
 
 <style type="text/css">
+#header {color: red;}
 label.choice:hover {background-color: yellow; cursor:pointer; cursor:hand;}
 input:focus {background: yellow; }
 #comment-end-input {width: 500px; }
 label.error { float: none; color: red; padding-left: .5em; vertical-align: top; display:none; }
 </style>
 
-<form id='submit-view'>
 
+<div id="header"></div>
+
+<form id='followup-form'>
 <div id='followup-view'>
+<div id='followup-desc'></div><br/>
 <?php
 foreach($followup['valid-question-ids'] as $i){
 echo "<label id='followup-".$i."-desc'></label>";
@@ -98,25 +108,46 @@ echo "<br /><br />";
 }
 ?>
 </div>
+</form>
 
-<div id='demography-view'>
+<form id='demography-form'>
+<div id='demography-desc'></div><br/>
+<?php
+foreach($demography['valid-question-ids'] as $i){
+echo "<label id='demography-".$i."-desc'></label>";
+echo '<label class="error" for="demography-'.$i.'" ><br />';
+    if($lang=='zh'){
+      echo '请选择一个答案.';
+    }else{
+      echo 'Please select an answer.';
+    }
+echo "</label><br />";
+  $count = count($demography['questions-answers'][$i]['answer'][$lang]);
+  for($j=1; $j<=$count; $j++){
+  echo '<label class="choice"><input type="radio" name="demography-'.$i.'" value="'.$j.'" /> <span class="choice" id="demography-'.$i.'-answer-'.$j.'" ></span>&nbsp&nbsp&nbsp&nbsp&nbsp</label><br/>';
+  }
+echo "<br /><br />";
+}
+?>
+</form>
 
-</div>
+<form id='final-form'>
 
-<div id='end-form'></div>
+<div id='end-desc'></div>
 <div id='confirmation-number'></div>
 <br/>
 
-<div id='email-form'></div>
+<div id='email-desc'></div>
 <input id='email' name='email' type="text"/>
 <br /><br />
 
   <label id='comment-end-label'></label><br />
   <input id='comment-end-input' type="text" /><br />
 
-<input id='submit' type="button"/>
 
 </form>
+
+<input id='submit' type="button"/>
 
 <div id='result'></div>
 <div id='final-view'></div>
