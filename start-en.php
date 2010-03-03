88b77a81cf060da0b914e114d205d0d692697908
Yi Wang <wajulia@gmail.com>
2010-03-02 22:40:00 -0500

    rename - to . in start files

--- start-en.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once('conf.d/server/ga.php');
-?>
-<script type="text/javascript">
-
-//location.href = 'consent.php?lang=en';
-location.replace('consent.php?lang=en');
-//window.open('consent.php?lang=en','Survey');
-</script>
--- start-zh.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once('conf.d/server/ga.php');
-?>
-
-<script type="text/javascript">
-
-location.href = 'consent.php?lang=zh';
-
-</script>
