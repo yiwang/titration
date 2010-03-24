7d7cf6f5bdec4ab2e71b7913b9aeea15e63b037a
Yi Wang <e@yi-wang.me>
2010-03-24 17:08:34 -0400

    add all-ids.yaml, combine.sh, id field for each

---
+++ conf.d/questions/combine.sh
@@ -0,0 +1,2 @@
+cat 1.yaml 2.yaml 3.yaml 4.yaml 5.yaml 6.yaml 7.yaml 8.yaml 9.yaml 10.yaml 11.yaml 12.yaml 13.yaml 14.yaml 15.yaml 16.yaml 17.yaml 18.yaml 19.yaml 20.yaml 21.yaml 22.yaml 23.yaml 24.yaml > all-ids.yaml
+
